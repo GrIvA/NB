ed9b7e91b65b49ba20eb0e506506b62b9a262d25
GrIvA <griva99@i.ua>
2012-07-18 10:51:48 +0300

    Small tool for ADVHash

---
+++ advCorrect.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import pickle
+
+vADVHashPath = 'Trash/ADVHash'
+
+if __name__ == '__main__':
+    if os.path.exists(vADVHashPath): 
+        f = open(vADVHashPath, "rb")
+        ADVHash = pickle.load(f)
+        f.close()
+        print 'Load ADV array...'
+    else:
+    	print "Nothing ADV array... sorry"
+    	exit()
+
+    for a in ADVHash:
+    	if ADVHash[a][0] == '*':
+    		print '%s(%s)' % (ADVHash[a][1], ADVHash[a][2])
+    		ADVHash[a][0] = raw_input("accept advertising? (Y/N)").upper()
+
+    f = open(vADVHashPath, "wb")
+    pickle.dump(ADVHash, f)
+    f.close()
+
